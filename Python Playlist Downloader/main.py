d654c4289c8bb3897966b2e15c5d14e7f77893d9
Aarav <ab1234now@gmail.com>
2023-11-26 00:14:19 -0500

    New tests for system

--- Python Playlist Downloader/main.py
+++ Python Playlist Downloader/main.py
@@ -6,17 +6,24 @@
 ██║██║ ╚═╝ ██║██║     ╚██████╔╝██║  ██║   ██║   ███████║
 ╚═╝╚═╝     ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝
 """
-import yt_dlp
 import os
 import time
-import tkinter as tk
-from tkinter import filedialog, ttk
+import yt_dlp
 import datetime
 import threading
-from PIL import Image
-from mutagen.mp3 import MP3
-from mutagen.flac import FLAC
-from mutagen.id3 import ID3, TIT2, TPE1, TALB, TDRC, APIC
+
+from PIL import *
+
+import tkinter
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+
+from mutagen.mp3 import *
+from mutagen.id3 import APIC, ID3, TIT2, TPE1, TALB, TDRC
+from mutagen.flac import *
+
 
 """
     ███╗   ███╗██████╗ ██████╗     ███████╗███████╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗
@@ -404,8 +411,8 @@ def ActionCheckboxChange():
 
 # Function to open file explorer to select directory
 def selectDir():
-    directory = filedialog.askdirectory()
-    directoryBox.delete(0, tk.END)  # Clear previous entry
+    directory = askdirectory()
+    directoryBox.delete(0, END)  # Clear previous entry
     directoryBox.insert(0, directory)
 
 # Function to update progress bar
@@ -420,7 +427,7 @@ def startDownload():
     directory_path = directoryBox.get()
     url_link = urlBox.get()     
 
-    progress_var = tk.DoubleVar()
+    progress_var = DoubleVar()
     progress_bar.configure(variable=progress_var)
     progress_var.set(0)
     
@@ -442,89 +449,131 @@ def startDownload():
 
 
 # Main application window
-mainframe = tk.Tk()
-mainframe.geometry("800x450")
+mainframe = Tk()
+mainframe.geometry("960x540")
 mainframe.resizable(False, False)
-mainframe.title("YTPFLACU")
+mainframe.title("YTtF")
+mainframe.configure(bg="#D3D3D3")  # Set background color to light gray
 
 # Global variables
 checkboxStates = [True, True, True, True]
 dropdownOption = ''
 
 # Assigns all of the values for the grid. 'i' is the width, 16, and 'j' is the height, 9.
-style = ttk.Style()
-style.configure('CustomFrame.TFrame', background='black', relief='solid', borderwidth=1)
-frames = {(i, j): ttk.Frame(mainframe, height=50, width=50, relief='solid', style='CustomFrame.TFrame') for i in range(16) for j in range(9)}
+style = Style()
+style.configure('CustomFrame.TFrame', background='#373f51')
+frames = {(i, j): Frame(mainframe, height=540/9, width=960/16, style='CustomFrame.TFrame') for i in range(16) for j in range(9)}
 for i, j in frames:
     frames[i, j].grid(row=j, column=i)
 
 # Widgets and UI elements
-titleLabel = tk.Label(mainframe, text="YouTube Playlist to .flac Utility (YTPFLACU)", font=("MS Sans Serif", 22, 'bold'))
-titleLabel.grid(row=0, column=2, columnspan=12, rowspan=2)
+titleLabel = Label(mainframe, text="YouTube to File Downloader (YTtF)", font=("Segoe UI", 22, 'bold'), background='#373f51', foreground='#d8dbe2')
+titleLabel.grid(row=0, column=2, columnspan=12, rowspan=3)
 
-style = ttk.Style()
-style.configure("Horizontal.TSeparator", background="green")
-horizontalSeparator = ttk.Separator(mainframe, orient='horizontal', style="Horizontal.TSeparator")
+style = Style()
+style.configure("Horizontal.TSeparator", background="#808080")  # Set separator color to gray
+horizontalSeparator = Separator(mainframe, orient='horizontal', style="Horizontal.TSeparator")
 horizontalSeparator.grid(row=1, column=1, columnspan=14, rowspan=2, sticky='ew')
 
-style = ttk.Style()
-style.configure("Vertical.TSeparator", background="green")
-verticalSeparator = ttk.Separator(mainframe, orient='vertical', style="Vertical.TSeparator")
+style = Style()
+style.configure("Vertical.TSeparator", background="#808080")  # Set separator color to gray
+verticalSeparator = Separator(mainframe, orient='vertical', style="Vertical.TSeparator")
 verticalSeparator.grid(row=2, column=7, columnspan=2, rowspan=6, sticky='ns')
 
-urlLabel = ttk.Label(mainframe, text="Playlist URL:", font=("MS Sans Serif", 11))
+urlLabel = Label(mainframe, text="Playlist URL:", font=("Segoe UI", 11, 'bold'), background='#373f51', foreground='#d8dbe2')
 urlLabel.grid(row=2, column=0, columnspan=3)
-urlBox = ttk.Entry(mainframe, width=40)
+urlBox = Entry(mainframe, width=40)
 urlBox.grid(row=2, column=2, columnspan=6)
 
-directoryLabel = ttk.Label(mainframe, text="Output directory:", font=("MS Sans Serif", 11))
+directoryLabel =  Label(mainframe, text="Output directory:", font=("System", 11), background='#D3D3D3')
 directoryLabel.grid(row=3, column=0, columnspan=3)
-directoryButton = ttk.Button(mainframe, text="Select through Explorer", command=selectDir)
+directoryButton = Button(mainframe, text="Select through Explorer", command=selectDir, style="TButton")  # Add a style for button
 directoryButton.grid(row=4, column=2, columnspan=6)
-directoryBox = ttk.Entry(mainframe, width=37)
+directoryBox = Entry(mainframe, width=37)
 directoryBox.grid(row=3, column=2, columnspan=6)
 
-selected_option = tk.StringVar()    
-dropdown_label = ttk.Label(mainframe, text="Select a file type:", font=("MS Sans Serif", 11))
+selected_option = StringVar()    
+dropdown_label = Label(mainframe, text="Select a file type:", font=("System", 11), background='#D3D3D3')
 dropdown_label.grid(row=5, column=0, columnspan=3)
 
 options = [".mp3", ".flac"]
-dropdown = ttk.Combobox(mainframe, textvariable=selected_option, values=options)
+dropdown = Combobox(mainframe, textvariable=selected_option, values=options)
 dropdown.grid(row=5, column=2, columnspan=6)
 dropdown.bind("<<ComboboxSelected>>", actionDropdownSelect)
 
-checkbox_values = [tk.BooleanVar() for _ in range(4)]
-checkbox_frame = ttk.Frame(mainframe)
+checkbox_values = [BooleanVar() for _ in range(4)]
+checkbox_frame = Frame(mainframe)
 checkbox_frame.grid(row=6, column=2, columnspan=6, rowspan=2)
 
-checkbox_label = ttk.Label(mainframe, text="Select what you want:", font=("MS Sans Serif", 11))
+checkbox_label = Label(mainframe, text="Select what you want:", font=("System", 11), background='#D3D3D3')
 checkbox_label.grid(row=6, column=0, columnspan=3, rowspan=2)
 
-checkbox1_var = tk.BooleanVar()
-checkbox1 = ttk.Checkbutton(checkbox_frame, text="Checkbox 1", variable=checkbox1_var, command=ActionCheckboxChange)
+checkbox1_var = BooleanVar()
+checkbox1 = Checkbutton(checkbox_frame, text="Checkbox 1", variable=checkbox1_var, command=ActionCheckboxChange)
 checkbox1.grid(row=0, column=0, padx=5, pady=5)
 
-checkbox2_var = tk.BooleanVar()
-checkbox2 = ttk.Checkbutton(checkbox_frame, text="Checkbox 2", variable=checkbox2_var, command=ActionCheckboxChange)
+checkbox2_var = BooleanVar()
+checkbox2 = Checkbutton(checkbox_frame, text="Checkbox 2", variable=checkbox2_var, command=ActionCheckboxChange)
 checkbox2.grid(row=0, column=1, padx=5, pady=5)
 
-checkbox3_var = tk.BooleanVar()
-checkbox3 = ttk.Checkbutton(checkbox_frame, text="Checkbox 3", variable=checkbox3_var, command=ActionCheckboxChange)
+checkbox3_var = BooleanVar()
+checkbox3 = Checkbutton(checkbox_frame, text="Checkbox 3", variable=checkbox3_var, command=ActionCheckboxChange)
 checkbox3.grid(row=1, column=0, padx=5, pady=5)
 
-checkbox4_var = tk.BooleanVar()
-checkbox4 = ttk.Checkbutton(checkbox_frame, text="Checkbox 4", variable=checkbox4_var, command=ActionCheckboxChange)
+checkbox4_var = BooleanVar()
+checkbox4 = Checkbutton(checkbox_frame, text="Checkbox 4", variable=checkbox4_var, command=ActionCheckboxChange)
 checkbox4.grid(row=1, column=1, padx=5, pady=5)
 
 # Start button event handler
-start_button = ttk.Button(mainframe, text="Activate Program", command=startDownload)
+start_button = Button(mainframe, text="Activate Program", command=startDownload, style="TButton")  # Add a style for button
 start_button.grid(row=5, column=10, columnspan=4)
 
 # Progress bar
-progress_bar = ttk.Progressbar(mainframe, orient="horizontal", length=250, mode="determinate")
-progress_bar.grid(row=4, column=9, columnspan=6)
+progress_bar = Progressbar(mainframe, orient="horizontal", length=250, mode="determinate").grid(row=4, column=9, columnspan=6)
 
+
+def show_disclaimer():
+    disclaimer_text = """\
+Petrichor Systems - Disclaimer and License Notice
+
+By using Petrichor Systems software "YouTube to File Downloader (YTtF)", you agree to the following terms:
+
+1. No Liability: The Software is provided "as is" and without any warranty, either express or implied. Petrichor Systems, its author, and contributors will not be held liable for any damages, including but not limited to direct, indirect, special, incidental, or consequential damages arising out of the use or inability to use the Software.
+
+2. No Responsibility for Data Loss: Petrichor Systems, its author, and contributors are not responsible for any data loss that may occur as a result of using the Software. It is the user's responsibility to regularly backup their data.
+
+3. All Rights Reserved: The Software is protected by copyright and other intellectual property laws. All rights are reserved by Petrichor Systems. You may not distribute, modify, or reproduce the Software in any form without the express written permission of Petrichor Systems.
+
+4. No Support: Petrichor Systems, its author, and contributors are under no obligation to provide support, maintenance, or updates for the Software.
+
+5. Governing Law: This agreement shall be governed by and construed in accordance with the laws of the United States of America.
+
+By using the Software, you acknowledge that you have read, understood, and agreed to be bound by the terms and conditions of this disclaimer and license notice.
+
+PetrichorDawn
+Email: ab123456now@gmail.com
+Petrichor Systems
+"""
+
+    tkinter.messagebox.showinfo("Disclaimer", disclaimer_text)
+
+
+discstyle = Style()
+discstyle.configure('TButton', font=('Segoe UI', 11, 'bold'))
+
+# Create a button to show the disclaimer
+disclaimer_button = Button(mainframe, text="Show Disclaimer", command=show_disclaimer, style='TButton')
+disclaimer_button.grid(row=0, column=1, rowspan=3, columnspan=3, padx=5, pady=5)
+
+"""
+phone = tk.StringVar()
+home = ttk.Radiobutton(mainframe, text='Home', variable=phone, value='home')
+home.grid(row=1, column=2, padx=5, pady=5)
+office = ttk.Radiobutton(mainframe, text='Office', variable=phone, value='office')
+office.grid(row=1, column=3, padx=5, pady=5)
+cell = ttk.Radiobutton(mainframe, text='Mobile', variable=phone, value='cell')
+cell.grid(row=1, column=4, padx=5, pady=5)
+"""
 # Run the application
 if __name__ == '__main__':
     mainframe.mainloop()
-
